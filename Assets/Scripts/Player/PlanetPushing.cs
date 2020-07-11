7ccce9349a9effa5ace73dd89538b141d331118e
sem laan <semlaan@dds.nl>
2020-07-11 17:54:06 +0200

    Made a class that reads the left mouse button input

---
+++ Assets/Scripts/Player/PlanetPushing.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPushing : MonoBehaviour
+{
+
+    private Controls controls;
+    private bool leftMousePressedLastFrame = false;
+    
+
+
+    private void Awake()
+    {
+
+        controls = new Controls();
+        controls.Enable();
+    }
+
+
+    private void Update()
+    {
+
+        bool leftMousePressed = controls.Gameplay.leftMouse.ReadValue<float>() == 1;
+        Vector2 mousePosition = controls.Gameplay.mousePosition.ReadValue<Vector2>();
+
+        if (leftMousePressed && !leftMousePressedLastFrame)
+        {
+
+            Collider2D collision = Physics2D.OverlapPoint(Camera.main.ScreenToWorldPoint(mousePosition));
+            Destroy(collision.gameObject);
+        }
+
+        leftMousePressedLastFrame = leftMousePressed;
+    }
+}
