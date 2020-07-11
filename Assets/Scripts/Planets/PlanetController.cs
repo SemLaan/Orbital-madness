f2c9d3b91658cac7a3539e7bcc9df937eb37aa70
sem laan <semlaan@dds.nl>
2020-07-11 11:00:41 +0200

    Planets now start with an initial velocity

--- Assets/Scripts/Planets/PlanetController.cs
+++ Assets/Scripts/Planets/PlanetController.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class PlanetController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    [SerializeField] private Vector2 initialVelocity;
+
+    private Vector2 velocity;
+
+
+
+    private void Awake()
     {
-        
+
+        velocity = initialVelocity;
     }
 }
