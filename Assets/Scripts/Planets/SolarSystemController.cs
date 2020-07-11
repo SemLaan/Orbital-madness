56903c4aa96ac9a6a42ade726112f7ee82d3db7e
sem laan <semlaan@dds.nl>
2020-07-11 12:31:19 +0200

    Added a parent object for all the planets in the solar system and made a struct for planet information

---
+++ Assets/Scripts/Planets/SolarSystemController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Struct for telling the planets where all the other planets are
+public struct PlanetData 
+{
+
+    public Vector3 position;
+    public float mass;
+
+    public PlanetData(Vector3 position, float mass)
+    {
+
+        this.position = position;
+        this.mass = mass;
+    }
+}
+
+
+public class SolarSystemController : MonoBehaviour
+{
+
+    List<GameObject> planets; // All the planet objects
+    List<PlanetData> planetLocations; // Updates every fixed update with all the locations and masses of the planets
+
+
+    private void Awake()
+    {
+        
+        //TODO: grab children
+    }
+
+
+
+    private void FixedUpdate()
+    {
+        
+        //TODO: make list of planet data
+        //TODO: call planets update velocity functions
+        //TODO: call planets update position functions
+    }
+}
