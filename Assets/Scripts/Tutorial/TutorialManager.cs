7865f28351f8ec81f2a1c6c16785302968a92969
sem laan <semlaan@dds.nl>
2020-07-12 15:51:04 +0200

    Made a class that is going to activate tutorial cards based on the timer

---
+++ Assets/Scripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+
+    [SerializeField] private float launchPlanet = 0, pan = 0, timer = 0;
+    [SerializeField] private GameObject launchPlanetCard, panningCard, timerCard;
+    // TODO make connection to timer
+
+
+
+    private void Update()
+    {
+        
+        //TODO activate cards
+    }
+}
