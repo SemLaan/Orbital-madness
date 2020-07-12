97237b794fc33cd322dc26b7290cee3704ae5ef0
JosephOliai <josepholiai@gmail.com>
2020-07-12 11:31:57 +0200

    Made a script that switches scenes

---
+++ Assets/Scripts/Scenes/SceneSwitcher.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    [SerializeField] private int sceneIndex;
+
+    public void playGame()
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
